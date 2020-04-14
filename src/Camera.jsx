6f90121e6aaa8b9aac5ab0a05422172c0cef04b0
Krivokon <vladislav.krivokon@3dlook.me>
2020-04-14 13:49:20 +0300

    test commit

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -322,6 +322,7 @@ class Camera extends Component {
           ) : null}
 
           <div className={classNames('widget-camera-controls')}>
+
             {this.before(!processing
                 && (
                     // TODO enable this.takePhoto after feature approve
