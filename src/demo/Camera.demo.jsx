b0ccbc2dee16472893d964b244ad09c526b3dc94
tsyvinska <ilona.tsyvinska@3dlook.me>
2024-06-13 17:16:12 +0300

    feat: KDL-1243 postMessage added

--- src/demo/Camera.demo.jsx
+++ src/demo/Camera.demo.jsx
@@ -1,93 +1,135 @@
 import { h, Component, render } from "preact";
 import Camera from "../Camera.jsx";
 
-export default class App extends Component {
-  state = {
-    cameraOn: false,
-    type: null,
-    hardValidation: { front: null, side: null },
-    frontImage: null,
-    sideImage: null,
-  };
+class App extends Component {
+    state = {
+        cameraOn: false,
+        type: null,
+        hardValidation: { front: null, side: null },
+        isTableFlow: false,
+        frontImage: null,
+        sideImage: null,
+    };
 
-  saveFrontFile = (file) => {
-    console.log("saveFrontFile", file);
-    this.setState({
-      type: null,
-      frontImage: file,
-    });
-  };
 
-  saveSideFile = (file) => {
-    console.log("saveSideFile", file);
-    this.setState({
-      type: null,
-      sideImage: file,
-    });
-  };
+    resetState() {
+        this.setState({
+            cameraOn: false,
+            type: null,
+            hardValidation: { front: null, side: null },
+            isTableFlow: false,
+            frontImage: null,
+            sideImage: null,
+        });
+    }
 
-  disableTableFlow = () => {
-    console.log("disableTableFlow");
-  };
 
-  turnOffCamera = () => {
-    console.log("turnOffCamera");
-  };
+    componentWillMount() {
+        window.addEventListener('message', (event) => {
+            if (!event.data.hello) {
+                console.log(event.data);
+                const { type, isTableFlow, hardValidation } = event.data;
+                if (type && (type === 'front') || (type === 'side')) {
 
-  setDeviceCoordinates = (coords) => {
-    console.log("setDeviceCoordinates", coords);
-  };
+                    this.resetState();
+                    this.setState({
+                        type,
+                        isTableFlow,
+                        hardValidation: hardValidation ? hardValidation : { front: null, side: null },
+                    });
 
-  toggleCamera = (type) => () => {
-    this.setState({ type });
-  };
+                    console.log(event.data);
+                }
+            }
+        }, false);
+    }
 
-  render() {
-    const {
-      type,
-      hardValidation,
-      frontImage,
-      sideImage,
-    } = this.state;
 
-    return (
-      <div>
-        <h1>Camera</h1>
-        <button type="button" onClick={this.toggleCamera("front")}>
-          Take front foto
-        </button>
-        <button type="button" onClick={this.toggleCamera("side")}>
-          Take side foto
-        </button>
-        <div>
-          <p>Front image:</p>
-          {(frontImage) ? (
-            <img src={frontImage} alt="Front photo"/>
-          ) : (
-            <p>None</p>
-          )}
-          <p>Side image:</p>
-          {(sideImage) ? (
-            <img src={sideImage} alt="Side photo"/>
-          ) : (
-            <p>None</p>
-          )}
-        </div>
-        {type && (
-          <Camera
-            type={type}
-            saveFront={this.saveFrontFile}
-            saveSide={this.saveSideFile}
-            isTableFlow={false}
-            hardValidation={hardValidation}
-            disableTableFlow={this.disableTableFlow}
-            turnOffCamera={this.turnOffCamera}
-            setDeviceCoordinates={this.setDeviceCoordinates}
-          />
-        )}
-      </div>
-    );
-  }
+    sendMessage(type) {
+        const {
+            frontImage,
+            sideImage,
+        } = this.state;
+
+        window.postMessage({ eventType: `save ${type} file`, front: frontImage, side: sideImage });
+        window.ReactNativeWebView.postMessage({ eventType: `save ${type} file`, front: frontImage, side: sideImage });
+    }
+
+
+    saveFrontFile = (file) => {
+        const {
+            hardValidation,
+            isTableFlow,
+        } = this.state;
+
+        console.log("saveFrontFile", file);
+        this.setState({
+            type: null,
+            frontImage: file,
+        });
+
+        if (isTableFlow) {
+            if (!(hardValidation.front && !hardValidation.side)) {
+                this.setState({
+                    type: 'side',
+                });
+            }
+        } else {
+            setTimeout(() => this.sendMessage('front'), 500);
+        }
+    };
+
+    saveSideFile = (file) => {
+        console.log("saveSideFile", file);
+        this.setState({
+            type: null,
+            sideImage: file,
+        });
+
+        setTimeout(() => this.sendMessage('side'), 500);
+    };
+
+    disableTableFlow = () => {
+        console.log("disableTableFlow");
+    };
+
+    turnOffCamera = () => {
+        console.log("turnOffCamera");
+    };
+
+    setDeviceCoordinates = (coords) => {
+        console.log("setDeviceCoordinates", coords);
+    };
+
+    toggleCamera = (type) => () => {
+        this.setState({ type });
+    };
+
+    render() {
+        const {
+            type,
+            hardValidation,
+            isTableFlow,
+        } = this.state;
+
+        return (
+            <div>
+                {type && (
+                    <Camera
+                        type={type}
+                        saveFront={this.saveFrontFile}
+                        saveSide={this.saveSideFile}
+                        isTableFlow={isTableFlow}
+                        hardValidation={hardValidation}
+                        disableTableFlow={this.disableTableFlow}
+                        turnOffCamera={this.turnOffCamera}
+                        setDeviceCoordinates={this.setDeviceCoordinates}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
 render(<App />, document.body);
+export default App;
